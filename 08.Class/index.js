251cefcf32eba54d568afd85b63ce84c1f4017b7
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-05 10:53:55 +0530

    Learn about CRUD opeartion

--- 08.Class/index.js
+++ 08.Class/index.js
@@ -9,7 +9,27 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", function (req, res) {
-    res.render("index")
+    fs.readdir("./files",function(err,file){
+       res.render("index",{file:file})
+    })
+
+})
+app.post("/create", function (req, res) {
+   fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,function(err){
+     res.redirect("/")
+   })
 })
 
+app.get("/files/:filename",function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err,data){
+        res.render("show",{title:req.params.filename, details:data})
+    })
+})
+
+
+app.get("/files/:filename/delete",function(req,res){
+    fs.unlink(`./files/${req.params.filename}`,function(err){
+        res.redirect("/")
+    })
+})
 app.listen(3000)
