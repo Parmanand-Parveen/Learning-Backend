601dd39686016af72d3f74bfd8eb9a0a5a9892f4
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-17 01:33:27 +0530

    Tailwind added

--- 19.Class/app.js
+++ 19.Class/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app =  express();
+const path = require("path");
+const cookieParser = require("cookie-parser")
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser())
+app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("index");
 });
+
+app.get("/signup",(req,res)=>{
+    res.render("signup")
+})
 
 
 
