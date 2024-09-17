544c2e3d469b50060f9eeb4341bdbdf66200e25f
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-17 13:25:25 +0530

    FIles initilized

---
+++ 21.Class/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Server started successfully")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server started at http://localhost:3000/")
+})
